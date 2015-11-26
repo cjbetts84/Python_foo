f2c28210ceb3ecc317061975cba154480b32b66c
C. Betts <cjbetts84@gmail.com>
2015-11-26 09:28:38 -0500

    Update tic-tac.py
    Added ability to draw the board

--- tic-tac.py
+++ tic-tac.py
@@ -3,3 +3,13 @@
 #The book's complete example is at http://inventwithpython.com/chapter10.html
 #This is my own version.
 
+#dictionary to represent board, as described by book
+board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+#displays the tic-tac board
+def drawBoard():
+  print(board['top-L'] + " | " + board['top-M'] + " | " + board['top-R'])
+  print("--+---+--")
+  print(board['mid-L'] + " | " + board['mid-M'] + " | " + board['mid-R'])
+  print("--+---+--")
+  print(board['low-L'] + " | " + board['low-M'] + " | " + board['low-R'])
