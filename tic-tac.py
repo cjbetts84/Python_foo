f756f0084a659b0361e5c4058f0c0bc2447fbcc9
C. Betts <cjbetts84@gmail.com>
2015-11-26 09:56:23 -0500

    Update tic-tac.py
    1. Added import random
    2. Added a way to check if a space is available
    3. Began the logic of a computer's turn by just picking a random location; still need to try to win and try to block the user

--- tic-tac.py
+++ tic-tac.py
@@ -3,8 +3,10 @@
 #The book's complete example is at http://inventwithpython.com/chapter10.html
 #This is my own version.
 
+import random
+
 #dictionary to represent board, as described by book
-board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': 'X', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
 #displays the tic-tac board
 def drawBoard():
@@ -13,3 +15,20 @@ def drawBoard():
   print(board['mid-L'] + " | " + board['mid-M'] + " | " + board['mid-R'])
   print("--+---+--")
   print(board['low-L'] + " | " + board['low-M'] + " | " + board['low-R'])
+
+#Checks to see if a space is clear
+def isOpenSpace(space):
+  return board.get(space) == ' '
+
+#Computer's turn (try to win, then try to block, then just place random)
+def computerTurn():
+  #Place random X
+  while True:
+    #list(board.keys()) because dictionaries not indexed
+    randLocation = random.choice(list(board.keys()))
+    if isOpenSpace(randLocation):
+      break
+  board[randLocation] = 'X'
+
+  #Redisplay board
+  drawBoard()
