509cd194b7b5c8aca4212683f78729ffafea50b5
C. Betts <cjbetts84@gmail.com>
2015-11-26 09:17:31 -0500

    Create tic-tac.py
    Initial Commit

---
+++ tic-tac.py
@@ -0,0 +1,5 @@
+#Working through "Automate the Boring Stuff with Python" by Al Sweigart
+#Example / suggestion was to use a dictionary to simulate a tic-tac-toe board.
+#The book's complete example is at http://inventwithpython.com/chapter10.html
+#This is my own version.
+
