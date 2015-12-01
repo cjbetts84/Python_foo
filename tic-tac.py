3c42311731d4693d1037f767c838201021120246
C. Betts <cjbetts84@gmail.com>
2015-11-30 21:56:28 -0500

    Update tic-tac.py
    Corrected issue where computer would over-right the user's moves. 
    Changed the user input section around to be a little cleaner, as well as explain bad input.
    Still need to put it checks for winning, and computer's placement logic needs work

--- tic-tac.py
+++ tic-tac.py
@@ -6,9 +6,21 @@
 import random
 
 #dictionary to represent board, as described by book
-board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+         'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+         'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
-#displays the tic-tac board
+#Lists describing rows, columns, and diagonal lines for blocks and wins
+rows = [['top-L', 'top-M', 'top-R'],
+        ['mid-L', 'mid-M', 'mid-R'],
+        ['low-L', 'low-M', 'low-R']]
+cols = [['top-L', 'mid-L', 'low-L'],
+        ['top-M', 'mid-M', 'low-M'],
+        ['top-R', 'mid-R', 'low-R']]
+diags = [['top-L', 'mid-M', 'low-R'],
+         ['top-R', 'mid-M', 'low-L']]
+
+#Displays the tic-tac board
 def drawBoard():
   print(board['top-L'] + " | " + board['top-M'] + " | " + board['top-R'])
   print("--+---+--")
@@ -20,28 +32,162 @@ def drawBoard():
 def isOpenSpace(space):
   return (board.get(space) == ' ')
 
-#Computer will attempt a winning move
+#Computer will attempt to place a winning move.
+#If it can, it will return True as a flag to end the game
+#If it cannot win, it will return False
 def tryToWin():
-  #TO-DO
-  print("To do: implement logic to attempt a winning move.")
-  return False
+  xCount = 0
+  oCount = 0
+
+  #Check for win on a row
+  for r in rows:
+    for s in r:
+      if board.get(s) == 'X':
+        xCount += 1
+      elif board.get(s) == 'O':
+        oCount += 1
+      else:
+        openSpace = s
+    #end: for s in r
+
+    if xCount == 2 and oCount == 0:
+      board[openSpace] = 'X'
+      return True #Able to win
+    else:
+      xCount = 0
+      oCount = 0
+      openSpace = None
+  #end: for r in rows
+
+  #Check for win on a column
+  for c in cols:
+    for s in c:
+      if board.get(s) == 'X':
+        xCount += 1
+      elif board.get(s) == 'O':
+        oCount += 1
+      else:
+        openSpace = s
+    #end: for s in c
+
+    if xCount == 2 and oCount == 0:
+      board[openSpace] = 'X'
+      return True #Able to win
+    else:
+      xCount = 0
+      oCount = 0
+      openSpace = None
+  #end: for c in cols
+
+  #Check for win on a diagonal
+  for d in diags:
+    for s in d:
+      if board.get(s) == 'X':
+        xCount += 1
+      elif board.get(s) == 'O':
+        oCount += 1
+      else:
+        openSpace = s
+    #end: for s in d
+        
+    if xCount == 2 and oCount == 0:
+      board[openSpace] = 'X'
+      return True #Able to win
+    else:
+      xCount = 0
+      oCount = 0
+      openSpace = None
+  #end for d in diagonals
+      
+  return False #Unable to win
 
 #Computer will attempt to block the user from winning
+#Returns True if able, False if it cannot
 def tryToBlock():
-  #TO-DO
-  print("To do: implement logic to block user's winning move.")
-  return False
+  xCount = 0
+  oCount = 0
+
+  #Check to block user on a row
+  for r in rows:
+    for s in r:
+      if board.get(s) == 'O':
+        oCount += 1
+      elif board.get(s) == 'X':
+        xCount += 1
+      else:
+        openSpace = s
+    #end: for s in r
+
+    if xCount == 0 and oCount == 2:
+      board[openSpace] = 'X'
+      return True #Able to block
+    else:
+      xCount = 0
+      oCount = 0
+      openSpace = None
+  #end: for r in rows
+
+  #Check to block user on a column
+  for c in cols:
+    for s in c:
+      if board.get(s) == 'O':
+        oCount += 1
+      elif board.get(s) == 'X':
+        xCount += 1
+      else:
+        openSpace = s
+    #end: for s in c
+
+    if xCount == 0 and oCount == 2:
+      board[openSpace] = 'X'
+      return True #Able to block
+    else:
+      xCount = 0
+      oCount = 0
+      openSpace = None
+  #end: for c in cols
+
+  #Check for blocking a diagonal win
+  for d in diags:
+    for s in d:
+      if board.get(s) == 'O':
+        oCount += 1
+      elif board.get(s) == 'X':
+        xCount += 1
+      else:
+        openSpace = s
+    #end: for s in d
+
+    if xCount == 0 and oCount == 2:
+      board[openSpace] = 'X'
+      return True #Able to block
+    else:
+      xCount = 0
+      oCount = 0
+      openSpace = None
+  #end: for d in diags
+      
+  return False  #Unable to block
 
 #Computer will randomly pic a cell
 def placeRandomly():
+  #Loop until an open space is randomly chose
   while True:
-    #list(board.keys()) because dictionaries not indexed
     randLocation = random.choice(list(board.keys()))
+
+    #Break from while True when open space is selected
     if isOpenSpace(randLocation):
       break
+    
   board[randLocation] = 'X'
 
-#Computer's turn (try to win, then try to block, then just place random)
+#Computer's turn...........
+#The computer will attempt to take the middle if it is available. It
+#will then attempt to place a winning move. Being unable to win, it
+#will try to block the user from winning. If unable to win, or keep the
+#user from winning, it will then default to picking a random location.
+#A message will appear regarding the selection. The board will be redrawn.
+#Returns true if the computer was able to beat the user, otherwise false.
 def computerTurn():
   if isOpenSpace('mid-M'):
     board['mid-M'] = 'X'
@@ -62,28 +208,32 @@ def computerTurn():
     drawBoard()
     return False
 
+#User's turn............
+#The user will be prompted to enter a row and column to place a mark.
+#The user will continue to be prompted if the space chosen is unavailable
+#or if the input is invalid.
 def userTurn():
-
+  #Loop for user choosing an open space
   while True:
     uRow = input("Which row (top, mid, low)?")
-    while ((uRow != 'top') and (uRow != 'mid') and (uRow != 'low')):
-      uRow = input("Which row (top, mid, low)?")
-      
-    uCol = input("Which column (L, M, R)?")
-    uCol = uCol.upper() #force uppercase
-    while ((uCol != 'L') and (uCol != 'M') and (uCol != 'R')):
-      uCol = input("Which column (L, M, R)?")
-      uCol = uCol.upper() #force uppercase
-
-    #if uCol is lowercase, isOpenSpace() doesn't work properly
-    uSpace = uRow + "-" + uCol
-
-    if isOpenSpace(uSpace):
-      break
+    uCol = input("Which colum (l, m, r)?")
+    uSpace = uRow + "-" + uCol.upper()
+    if uSpace not in board:
+      print("I don't understand...")
+    else:
+      if not isOpenSpace(uSpace):
+        print("That space is already used.")
+      else:
+        break
 
   board[uSpace] = 'O'
   drawBoard()
 
+#Main game loop.........
+#Each player gets at most four turns before the game would end
+#as a draw or win/lose. If either userTurn() or computerTurn()
+#return True, that signals they have won the game, and the loop
+#will break.
 print("Starting the game.\n I'm playing 'X', you go first.")
 for i in range(4):
   userTurn()
